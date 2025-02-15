a80a2f77eb8d91a57fce048368d09408d635b1fc
IStasovskyi93 <stsovsky400m@gmail.com>
2025-02-15 19:28:35 +0100

    finished part3 of book

---
+++ primer/index.js
@@ -0,0 +1,9 @@
+let hatPrice = 100;
+console.log(`Hat price: ${hatPrice}`);
+let bootPrice = "100";
+console.log(`Boot price: ${bootPrice}`);
+console.log(`Type: ${typeof bootPrice}`);
+console.log(`Type: ${typeof hatPrice}`);
+let firstCity = 0;
+let secondCity = firstCity ?? "London";
+console.log(`City: ${secondCity}`);
